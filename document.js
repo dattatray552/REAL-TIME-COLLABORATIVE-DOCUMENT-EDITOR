2962c906bc84025276bb2ecfd12b42b36de92a9c
dattatray552 <suldattatray109@gmail.com>
2025-02-04 11:32:52 +0530

    Add files via upload

---
+++ document.js
@@ -0,0 +1,8 @@
+
+const mongoose = require('mongoose');
+
+const DocumentSchema = new mongoose.Schema({
+  content: { type: Array, default: [] },
+});
+
+module.exports = mongoose.model('Document', DocumentSchema);
